880b08688f225dbd9f67b8f1ac7a1eb463daebf6
chhotashun <shaanchandra29@gmail.com>
2025-04-01 23:39:52 +0100

    Added Assembly logic for Button interrupts and Morse code input buffer

--- Assignment_2/Assignment_2.c
+++ Assignment_2/Assignment_2.c
@@ -1,13 +1,53 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include <stdio.h>
 
 
-int main()
+// Declare the external variables so the compiler knows they exist:
+extern unsigned char morse_code_buffer[];
+
+void print_morse_code();
+
+// Declare the main assembly code entry point.
+void main_asm();
+
+// Initialise a GPIO pin – see SDK for detail on gpio_init()
+void asm_gpio_init(uint pin) {
+ gpio_init(pin);
+}
+
+// Set direction of a GPIO pin – see SDK for detail on gpio_set_dir()
+void asm_gpio_set_dir(uint pin, bool out) {
+ gpio_set_dir(pin, out);
+}
+
+// Get the value of a GPIO pin – see SDK for detail on gpio_get()
+bool asm_gpio_get(uint pin) {
+ return gpio_get(pin);
+}
+
+// Set the value of a GPIO pin – see SDK for detail on gpio_put()
+void asm_gpio_put(uint pin, bool value) {
+ gpio_put(pin, value);
+}
+
+// Enable falling-edge interrupt – see SDK for detail on gpio_set_irq_enabled()
+void asm_gpio_set_irq(uint pin) {
+    gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true);
+   }
+
+void print_morse_code()
 {
-    stdio_init_all();
+    printf("Morse code: %s\n", morse_code_buffer);
+}
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+/*
+ * Main entry point for the code - simply calls the main assembly function.
+ */
+ int main() {
+    stdio_init_all(); // Initialise all basic IO
+    printf("Assignment #2...\n"); // Basic print to console
+    main_asm();
+    return 0;
 }
