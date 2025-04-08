398b522aa904d75e78ff8a0748866585523c0343
merkelmarrow <marco.a.blackwell@gmail.com>
2025-04-08 03:10:32 +0100

    feat: add new_char_flag to assembly code

--- src/main.c
+++ src/main.c
@@ -1,16 +1,20 @@
+#include <hardware/sync.h>
 #include <pico/stdio.h>
 #include <pico/time.h>
 #include <stdint.h>
+#include <stdio.h>
 
 #include "display_utils.h"
+#include "sdk_asm_helpers.c"
 
 // globals
 const uint32_t MORSE_BUFFER_SIZE = 1024;
 
 // declare the external variables so the compiler knows they exist:
-extern unsigned char morse_code_buffer[];
-extern uint32_t current;
-extern uint32_t flag;
+extern volatile unsigned char morse_code_buffer[];
+extern volatile uint32_t current;
+extern volatile uint32_t sequence_complete_flag;
+extern volatile uint32_t new_char_flag;
 
 // Declare the main assembly code entry point.
 void main_asm();
@@ -21,6 +25,16 @@ void main_menu();
 int main() {
   stdio_init_all(); // Initialise all basic IO
   sleep_ms(5000);   // time to allow serial monitor connection
+
+  main_asm();
+
+  while (true) {
+    if (new_char_flag) {
+      new_char_flag = 0;
+      printf(" <- character entered.\n");
+    } else
+      __wfi();
+  }
 
   return 0;
 }
